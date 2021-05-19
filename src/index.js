6b92f3538eb26e907254f7e6aa1a8b181b60310b
Sheridan Kates <sheridan.kates@gmail.com>
2021-05-19 21:23:33 +0100

    Migrate to react-query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 import Map from "./Map";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (_failureCount, error) =>
+        error.response.status === 404 ? false : true,
+      refetchOnWindowFocus: false,
+      staleTime: 12 * 60 * 60 * 1000,
+    },
+  },
+});
 
 const Application = () => {
   return (
-    <>
-      <Map />
-    </>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Map />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </React.StrictMode>
   );
 };
 
